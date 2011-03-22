92ebcb14503c5c9326ee38c1ba5ec8721df7b52f
Ken Gilmer <kgilmer@gmail.com>
2011-03-22 17:34:02 +0900

    android: mkcard script now makes sd image bug20 style rather than beagleboard style.  One ext3 partition w/ all filesystem contents.
    Fixes: https://github.com/buglabs/android/issues#issue/2

--- scripts/mkcard.sh
+++ scripts/mkcard.sh
@@ -19,49 +19,46 @@ fi
 DRIVE=$1
 TARBALL=$2
 
+# Remove directories that may be hanging around from prevoius partial or erronous run.
+if [ -d /tmp/mp1 ]; then
+  rm -Rf /tmp/mp1
+fi
+
+if [ -d /tmp/mkcard ]; then
+  rm -Rf /tmp/mkcard 
+fi
+
+# Create temporary mount point
 mkdir /tmp/mp1
-mkdir /tmp/mp2
 
-dd if=/dev/zero of=$DRIVE bs=1024 count=1024
+/bin/umount ${DRIVE}1 > /dev/null 2>&1
+/bin/umount ${DRIVE}p1 > /dev/null 2>&1
+dd if=/dev/zero of=${DRIVE} bs=1024 count=1024
+echo "Partitioning Disk $DEVNAME"
+echo "d
+n
+p
+1
 
-SIZE=`fdisk -l $DRIVE | grep Disk | grep bytes | awk '{print $5}'`
 
-echo DISK SIZE - $SIZE bytes
-
-CYLINDERS=`echo $SIZE/255/63/512 | bc`
-
-echo CYLINDERS - $CYLINDERS
-
-{
-echo ,9,0x0C,*
-echo ,,,-
-} | sfdisk -D -H 255 -S 63 -C $CYLINDERS $DRIVE
+a
+1
+w
+" | fdisk ${DRIVE} > /dev/null 2>&1
+/bin/umount ${DRIVE}1 > /dev/null 2>&1
+/bin/umount ${DRIVE}p1 > /dev/null 2>&1
 
 sleep 1
 
 if [ -b ${DRIVE}1 ]; then
-	umount ${DRIVE}1
-	mkfs.vfat -F 32 -n "boot" ${DRIVE}1
-	BOOT_PARTITION=${DRIVE}1
+	umount ${DRIVE}1 > /dev/null 2>&1
+	mke2fs -j -L "root" ${DRIVE}1
+	ROOT_PARTITION=${DRIVE}1
 else
 	if [ -b ${DRIVE}p1 ]; then
-		umount ${DRIVE}p1
-		mkfs.vfat -F 32 -n "boot" ${DRIVE}p1
-		BOOT_PARTITION=${DRIVE}p1
-	else
-		echo "Cant find boot partition in /dev"
-	fi
-fi
-
-if [ -b ${DRIVE}2 ]; then
-	umount ${DRIVE}2
-	mke2fs -j -L "root" ${DRIVE}2
-	ROOT_PARTITION=${DRIVE}2
-else
-	if [ -b ${DRIVE}p2 ]; then
-		umount ${DRIVE}p2
-		mke2fs -j -L "root" ${DRIVE}p2
-		ROOT_PARTITION=${DRIVE}p2
+		umount ${DRIVE}p1 > /dev/null 2>&1
+		mke2fs -j -L "root" ${DRIVE}p1
+		ROOT_PARTITION=${DRIVE}p1
 	else
 		echo "Cant find rootfs partition in /dev"
 	fi
@@ -70,21 +67,19 @@ sleep 1
 
 set +e
 mkdir /tmp/mkcard
-echo "Extracting contents of image tarball..."
-mount $BOOT_PARTITION /tmp/mp1
-mount $ROOT_PARTITION /tmp/mp2
+echo "Extracting contents of image tarball to $ROOT_PARITION"
+mount $ROOT_PARTITION /tmp/mp1
 
 tar xfz $TARBALL -C /tmp/mkcard
 
 echo "Writing contents to sd card..."
-cp /tmp/mkcard/uImage /tmp/mp1/
-tar xf /tmp/mkcard/rootfs.tar -C /tmp/mp2 
+mkdir /tmp/mp1/boot
+cp /tmp/mkcard/uImage /tmp/mp1/boot/
+tar xf /tmp/mkcard/rootfs.tar -C /tmp/mp1
 
 sync
-umount $BOOT_PARTITION
 umount $ROOT_PARTITION
 sleep 1
 rmdir /tmp/mp1
-rmdir /tmp/mp2
 rm -Rf /tmp/mkcard
 echo "Completed succesfully, sd card is ready to be booted."
